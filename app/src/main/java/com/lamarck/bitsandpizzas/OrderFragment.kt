2d68713b45c193c9beacb9bee9df3bb7b0142d9b
yuran-nhassengo <yurannhassengo@gmail.com>
2022-06-25 17:47:15 +0200

    Implement viewBinding

--- app/src/main/java/com/lamarck/bitsandpizzas/OrderFragment.kt
+++ app/src/main/java/com/lamarck/bitsandpizzas/OrderFragment.kt
@@ -12,6 +12,7 @@ import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.chip.Chip
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.lamarck.bitsandpizzas.databinding.FragmentOrderBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,31 +25,24 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class OrderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentOrderBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val view =inflater.inflate(R.layout.fragment_order, container, false)
-        val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
-        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        //val view =inflater.inflate(R.layout.fragment_order, container, false)
+        //val toolbar = view.findViewById<MaterialToolbar>(R.id.toolbar)
+        _binding = FragmentOrderBinding.inflate(inflater,container,false)
+        val view = binding.root
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
 
-        val fab = view.findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener {
-            val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
-            val pizzaType = pizzaGroup.checkedRadioButtonId
+       // val fab = view.findViewById<FloatingActionButton>(R.id.fab)
+        binding.fab.setOnClickListener {
+            //val pizzaGroup = view.findViewById<RadioGroup>(R.id.pizza_group)
+            val pizzaType = binding.pizzaGroup.checkedRadioButtonId
 
             if (pizzaType==-1){
                 val text ="You need to choose a pizza type"
@@ -59,34 +53,21 @@ class OrderFragment : Fragment() {
                     else -> "Funghi pizza"
                 })
 
-                val parmesan = view.findViewById<Chip>(R.id.parmesan)
-                text += if (parmesan.isChecked) ",extras parmesan" else ""
-                val chiliOil = view.findViewById<Chip>(R.id.chili_oil)
-                text += if (chiliOil.isChecked) ", extras chili oil" else ""
-                Snackbar.make(fab,text,Snackbar.LENGTH_LONG).show()
+               // val parmesan = view.findViewById<Chip>(R.id.parmesan)
+                text += if (binding.parmesan.isChecked) ",extras parmesan" else ""
+                //val chiliOil = view.findViewById<Chip>(R.id.chili_oil)
+                text += if (binding.chiliOil.isChecked) ", extras chili oil" else ""
+                Snackbar.make(binding.fab,text,Snackbar.LENGTH_LONG).show()
             }
         }
 
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment OrderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            OrderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
     }
+
+
 }
